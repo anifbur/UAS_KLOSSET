10f1f473b9dfb0302866063ac379f6c668f491a1
anifbur <jamananip@gmail.com>
2025-08-02 21:32:48 +0700

    chart

--- klosset/src/main/java/com/example/klosset/controller/DashboardController.java
+++ klosset/src/main/java/com/example/klosset/controller/DashboardController.java
@@ -1,6 +1,7 @@
 package com.example.klosset.controller;
 
 import com.example.klosset.model.Asset;
+import com.example.klosset.model.JenisAsset;
 import com.example.klosset.repository.AssetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -9,6 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 public class DashboardController {
@@ -16,12 +19,23 @@ public class DashboardController {
     @Autowired
     private AssetRepository assetRepository;
 
-    // Tampilkan dashboard
    @GetMapping("/index")
 public String showDashboard(Model model, Authentication authentication) {
     List<Asset> assets = assetRepository.findAll();
+
+    
+    Map<String, Integer> assetCountByType = assets.stream()
+            .collect(Collectors.groupingBy(
+                    asset -> asset.getJenisAset().toString(),
+                    Collectors.summingInt(Asset::getQty)
+            ));
+
     model.addAttribute("assets", assets);
     model.addAttribute("username", authentication.getName());
+    model.addAttribute("assetCountByType", assetCountByType);
+
     return "index";
 }
+
+
 }
